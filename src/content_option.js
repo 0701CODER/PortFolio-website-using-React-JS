b29b422a7e73ce9cb5f20fe3dc1df9cd24022fb9
vasanth <maxdelz30@gmail.com>
2023-11-02 12:00:55 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,135 @@
+const logotext = "VASANTH";
+const meta = {
+    title: "Vasanth",
+    description: "I’m VASANTH _ Full stack devloper,currently working in Coimbatore",
+};
+
+const introdata = {
+    title: "I’m Vasanth",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop E-commerce Websites",
+    },
+    description: "Currently I'm working on #E-Commerce Website Development with 3 colleagues as crew members, under the guidance of #Product-owner. Currently we are following #Agile-methodology with #scrum & #CICD.",
+    your_img_url: "https://www.hypemax.com/site-img/banner/software-development.png",
+};
+
+const dataabout = {
+    title: "About MySelf",
+    aboutme: "Ambitious and computer science esteemed fresher who is capable of applying technical skills for the Improvement of the company. Improved my web development skills with the help of graduation in Computer Science and looking forward to work in a competitive environment that can boost my overall learning",
+};
+const worktimeline = [{
+    jobtitle: "Eshop-Ecommerce website using django",
+    where: "Appin Technologies",
+    date: "2023",
+},
+{
+    jobtitle: "Shopsite-Ecommerce Website using Django",
+    where: "Appin Technologies",
+    date: "2023",
+},
+{
+    jobtitle: "Portfolio-website using React Js",
+    where: "Appin Technologies",
+    date: "2023",
+},
+];
+
+const skills = [{
+    name: "Python",
+    value: 80,
+},
+{
+    name: "Django",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 75,
+},
+{
+    name: "React",
+    value: 60,
+},
+{
+    name: "CSS",
+    value: 90,
+},
+];
+
+const services = [{
+    title: "FrontEnd Development",
+    description: "In Frontend development involving working with various technologies and tools, in HTML5, CSS 3, BOOTSTRAP, JAVASCRIPT, JQUERY, ANGULAR JS, REACT JS.",
+},
+{
+    title: "Backend Development",
+    description: "In Backend development involving working with various technologies and tools, in PYTHON, JAVA, NODE-JS, PHP",
+},
+{
+    title: "Database and Framework Development",
+    description: "In Database and Framework development involving working with various technologies and tools, in MYSQL, DJANGO",
+},
+];
+
+const dataportfolio = [{
+    img: "https://fancycrave.com/wp-content/uploads/2019/08/Pizza-Shop-Photo-Pack-min-1024x681.jpg",
+    description: "A virtual pizza shop where the website displays different types of pizzas, customers can browse the catalog and select pizza and can remove pizza from their cart while also specifying the quantity of each pizza.",
+    link: "https://github.com/Vasanth272001/PizzaShop-Ecommerce-Website-using-Django.git",
+},
+{
+    img: "https://i.ytimg.com/vi/87j3ThdQrBI/maxresdefault.jpg",
+    description: "This is a memory puzzle game developed using Python and the Pygame library. The game features a visually appealing interface with a grid of cards, each with a hidden image.",
+    link: "https://github.com/Vasanth272001/Python-Mind-puzzles.git",
+},
+{
+    img: "https://wallpaperaccess.com/full/2484120.jpg",
+    description: " For the convenience of online shopping, a shopping cart is provided to the user.The system is implemented using Python's web framework Django.",
+    link: "https://github.com/Vasanth272001/Shopsite-Ecommerce-Website-using-Django.git",
+},
+{
+    img: "https://static.vecteezy.com/system/resources/previews/002/459/314/original/shopping-online-store-for-sale-mobile-ecommerce-3d-blue-background-shop-online-on-mobile-app-24-hours-shopping-cart-credit-card-minimal-store-online-device-3d-rendered-free-vector.jpg",
+    description: "This project deals with developing a Virtual website ‘E-commerce Website’. It provides the user with a list of the various products available for purchase in the store.",
+    link: "https://github.com/Vasanth272001/Maxcart-Ecommerce-website-using-Django.git",
+},
+{
+    img: "https://i.ibb.co/K56rGrK/to-do-list.png",
+    description: "React TodoList this is a very simple todo list application written using React.js",
+    link: "https://github.com/Vasanth272001/React-todolist.git",
+},
+{
+    img: "https://praveshpatel.com/wp-content/uploads/2021/08/developer.jpg",
+    description: " Portfolio’s design seeks to capture the visitors’ attention with a creative concept and seamless background animation. This portfolio is both well-written and informative, with easy access.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vasanthkumar272001@gmail.com",
+    YOUR_FONE: "+91 7094563848",
+    description: "Seeking a position  on Full Stack Web Development in a company where I can launch my career a valuable skill set ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
